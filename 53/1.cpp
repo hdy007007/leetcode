ae6fa4659d15313af8ae05860822ae378d84b0af
hdy007007 <hdy007193@hotmail.com>
2017-08-30 15:35:22 +0800

    Maximum Subarray

---
+++ 53/1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int global = nums[0],local = nums[0];
+        for (int i = 1;i < nums.size();i++){
+            local = max(nums[i],local + nums[i]);
+            global = max(global,local);
+        }
+        return global;
+    }
+};
